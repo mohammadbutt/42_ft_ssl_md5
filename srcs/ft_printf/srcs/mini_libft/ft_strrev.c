d7ece10b1e38bee5412a4c39ddb7fe9a7b3b1297
Mohammad Butt <mbutt@student.42.us.org>
2019-11-22 21:21:15 -0800

    Added ft_strrev and ft_swap in mini_libft

---
+++ srcs/ft_printf/srcs/mini_libft/ft_strrev.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/22 21:09:16 by mbutt             #+#    #+#             */
+/*   Updated: 2019/11/22 21:18:02 by mbutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini_libft.h"
+
+/*
+** function ft_strrev takes a string and reverses it in place
+*/
+
+char *ft_strrev(char *str)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = ft_strlen(str) - 1;
+	if (str)
+		while (i < len)
+			ft_swap(&str[i++], &str[len--]);
+	return (str);
+}
